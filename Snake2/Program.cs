9f0f19c79c0d018fb11829fd1a1bff991e7f0008
Nikamaliish <nikamaliish@mail.ru>
2016-06-13 15:22:30 +0300

    constructor Point

--- Snake2/Program.cs
+++ Snake2/Program.cs
@@ -10,16 +10,10 @@ namespace Snake2
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point();
-            p1.x = 3;
-            p1.y = 5;
-            p1.sym = '*';
+            Point p1 = new Point(3, 5, '*');
             p1.Draw();
 
-            Point p2 = new Point();
-            p2.x = 6;
-            p2.y = 2;
-            p2.sym = '#';
+            Point p2 = new Point(6, 2, '#');
             p2.Draw();
 
             Console.ReadLine();
